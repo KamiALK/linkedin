14ad011f7f006226ece77c9863a71836b857fcb7
kmialk <andresyalca@gmail.com>
2024-05-17 23:03:49 -0500

    tengo este error
    File
    "/home/kamilo/programming/linkedin/venv/lib/python3.12/site-packages/google/auth/_service_account_info.py",
    line 50, in from_dict    raise
    exceptions.MalformedError(google.auth.exceptions.MalformedError: Service
    account info was not in the expected format, missing fields token_uri.

---
+++ conection/Doc.py
@@ -0,0 +1,31 @@
+
+import os
+from dotenv import load_dotenv
+from google.oauth2.service_account import Credentials
+from googleapiclient.discovery import build
+
+# Cargar las variables de entorno desde el archivo .env
+load_dotenv()
+
+# Obtener las variables de entorno
+google_service_account_email = os.getenv("GOOGLE_SERVICE_ACCOUNT_EMAIL")
+google_private_key = os.getenv("GOOGLE_PRIVATE_KEY")
+document_id = os.getenv('GOOGLE_DOCUMENT_ID')
+
+# Crear las credenciales
+credentials = Credentials.from_service_account_info({
+    'type': 'service_account',
+    'client_email': google_service_account_email,
+    'private_key': google_private_key.replace('\\n', '\n')
+})
+
+# Construir el servicio de Google Docs
+service = build('docs', 'v1', credentials=credentials)
+
+# Obtener el contenido del documento
+document = service.documents().get(documentId=document_id).execute()
+document_content = document.get('body').get('content')
+
+# Imprimir el contenido del documento
+print('Contenido del documento:')
+print(document_content)
