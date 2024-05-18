46a00b28136d50469ca611bf7d5adcc06ad60333
kmialk <andresyalca@gmail.com>
2024-05-17 23:37:51 -0500

    agreegue la variable de entorno TOKEN_URI
    y tuve que agregar el correo del .dot al documento para que pudiera
    editar  "The caller does not have permission". Details: "The caller does
    not have permission" solo falto agregarlo a editores del documento

--- conection/Doc.py
+++ conection/Doc.py
@@ -11,12 +11,14 @@ load_dotenv()
 google_service_account_email = os.getenv("GOOGLE_SERVICE_ACCOUNT_EMAIL")
 google_private_key = os.getenv("GOOGLE_PRIVATE_KEY")
 document_id = os.getenv('GOOGLE_DOCUMENT_ID')
+token = os.getenv('TOKEN_URI')
 
 # Crear las credenciales
 credentials = Credentials.from_service_account_info({
     'type': 'service_account',
     'client_email': google_service_account_email,
-    'private_key': google_private_key.replace('\\n', '\n')
+    'private_key': google_private_key.replace('\\n', '\n'),
+    'token_uri': token
 })
 
 # Construir el servicio de Google Docs
