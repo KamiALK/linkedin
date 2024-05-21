b61244d3cf662fc54b9ee67f907131e51bbc9488
kmialk <andresyalca@gmail.com>
2024-05-21 01:06:28 -0500

    ya arregle el error y converti en diccionario cada texto

--- conection/Doc.py
+++ conection/Doc.py
@@ -27,42 +27,30 @@ service = build('docs', 'v1', credentials=credentials)
 # Obtener el contenido del documento
 document = service.documents().get(documentId=document_id).execute()
 document_content = document.get('body').get('content')
-print(document_content)
-print("hola mundo") 
-#
-# def extract_text_from_document(document_content):
-#     text = ""
-#     for content in document_content:
-#         if 'paragraph' in content:
-#             for element in content['paragraph']['elements']:
-#                 if 'textRun' in element:
-#                     text += element['textRun']['content']
-#         elif 'table' in content:
-#             for row in content['table']['tableRows']:
-#                 for cell in row['tableCells']:
-#                     for element in cell['content']:
-#                         if 'textRun' in element['paragraph']['elements'][0]:
-#                             text += element['paragraph']['elements'][0]['textRun']['content']
-#     return text
 
-# Utilizar la función para extraer el texto del documento
-# document_text = extract_text_from_document(document_content)
+def extract_text_from_document(document_content):
+    text = ""
+    index = 0
+    diccionario = {}
+    for content in document_content:
+        if 'paragraph' in content:
+            for element in content['paragraph']['elements']:
+                if 'textRun' in element:
+                    text += element['textRun']['content']
+                    index += 1
+                    diccionario[index] = element['textRun']['content']
+        # elif 'table' in content:
+        #     for row in content['table']['tableRows']:
+        #         for cell in row['tableCells']:
+        #             for element in cell['content']:
+        #                 if 'textRun' in element['paragraph']['elements'][0]:
+        #                     diccionario[index] = element['paragraph']['elements'][0]['textRun']['content']
+        #
+        #                     text += element['paragraph']['elements'][0]['textRun']['content']
+    return diccionario
 
-# Imprimir el texto extraído
-# print('Texto del documento:')
-# print(document_text)
+document_text = extract_text_from_document(document_content)
 
+print(document_text)
 
-import json
-print(document_content)
-
-analisis = json.loads(document_content)
-# Modificar el contenido del primer párrafo
-documento[1]['paragraph']['elements'][0]['textRun']['content'] = "Nuevo contenido"
-# Convertir de nuevo a JSON
-documento_modificado = json.dumps(documento)
-
-# Guardar el JSON modificado en un archivo
-with open('documento_modificado.json', 'w') as archivo:
-    archivo.write(documento_modificado)
 
