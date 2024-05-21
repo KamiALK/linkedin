6ead5cdbe843e2eec4a74cc06fd69d26350cd60b
kmialk <andresyalca@gmail.com>
2024-05-21 00:08:15 -0500

    error de manipulacion de json
    File "/home/kamilo/programming/linkedin/conection/Doc.py", line 59, in
    <module>    analisis = json.loads(document_content)
    ^^^^^^^^^^^^^^^^^^^^^^^^^^^^  File
    "/usr/lib/python3.12/json/__init__.py", line 339, in loads    raise
    TypeError(f'the JSON object must be str, bytes or bytearray, 'TypeError:
    the JSON object must be str, bytes or bytearray, not list

--- conection/Doc.py
+++ conection/Doc.py
@@ -29,25 +29,40 @@ document = service.documents().get(documentId=document_id).execute()
 document_content = document.get('body').get('content')
 print(document_content)
 print("hola mundo") 
-
-def extract_text_from_document(document_content):
-    text = ""
-    for content in document_content:
-        if 'paragraph' in content:
-            for element in content['paragraph']['elements']:
-                if 'textRun' in element:
-                    text += element['textRun']['content']
-        elif 'table' in content:
-            for row in content['table']['tableRows']:
-                for cell in row['tableCells']:
-                    for element in cell['content']:
-                        if 'textRun' in element['paragraph']['elements'][0]:
-                            text += element['paragraph']['elements'][0]['textRun']['content']
-    return text
+#
+# def extract_text_from_document(document_content):
+#     text = ""
+#     for content in document_content:
+#         if 'paragraph' in content:
+#             for element in content['paragraph']['elements']:
+#                 if 'textRun' in element:
+#                     text += element['textRun']['content']
+#         elif 'table' in content:
+#             for row in content['table']['tableRows']:
+#                 for cell in row['tableCells']:
+#                     for element in cell['content']:
+#                         if 'textRun' in element['paragraph']['elements'][0]:
+#                             text += element['paragraph']['elements'][0]['textRun']['content']
+#     return text
 
 # Utilizar la función para extraer el texto del documento
-document_text = extract_text_from_document(document_content)
+# document_text = extract_text_from_document(document_content)
 
 # Imprimir el texto extraído
-print('Texto del documento:')
-print(document_text)
+# print('Texto del documento:')
+# print(document_text)
+
+
+import json
+print(document_content)
+
+analisis = json.loads(document_content)
+# Modificar el contenido del primer párrafo
+documento[1]['paragraph']['elements'][0]['textRun']['content'] = "Nuevo contenido"
+# Convertir de nuevo a JSON
+documento_modificado = json.dumps(documento)
+
+# Guardar el JSON modificado en un archivo
+with open('documento_modificado.json', 'w') as archivo:
+    archivo.write(documento_modificado)
+
